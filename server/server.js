1d4f52c5cdf33c440014d780cddf7f700e7c3263
SubikshanM <subikshan.mailbox@gmail.com>
2026-02-04 16:40:20 +0530

    Resolved the backend issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://expense-tracker-pjf8.onrender.com'
   ],
   credentials: true
 }));
